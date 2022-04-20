f05076ddc60da53909b302b22a1239b8d8b38c72
kimhsn <hakimhsn06@gmail>
2022-04-21 01:58:07 +0200

    premiere question faite

--- src/main/java/fr/univrouen/rss22/controllers/IndexController.java
+++ src/main/java/fr/univrouen/rss22/controllers/IndexController.java
@@ -2,13 +2,33 @@ package fr.univrouen.rss22.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
 	
-	@GetMapping("/")
+	/* @GetMapping("/")
 	public String index() {
 		return "Hello rss22 !";
+	} */
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView mdav = new ModelAndView();
+		mdav.setViewName("index.html");
+		return mdav;
+	}
+	
+	@GetMapping("/help")
+	public ModelAndView help() {
+		ModelAndView mdav = new ModelAndView();
+		mdav.setViewName("help.html");
+		return mdav;
+	}
+	
+	@GetMapping("/rss22/resume/xml")
+	public String getListRSSinXML() {
+		return "";
 	}
 
 }
